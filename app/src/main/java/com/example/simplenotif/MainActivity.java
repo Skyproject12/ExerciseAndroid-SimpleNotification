1aa20095b177265156bdad9c3abb5966868e1c5d
sumendra12 <sumendrakadek29@gmail.com>
2019-10-13 00:41:05 +0700

    finish notification

--- app/src/main/java/com/example/simplenotif/MainActivity.java
+++ app/src/main/java/com/example/simplenotif/MainActivity.java
@@ -6,10 +6,14 @@ import androidx.core.app.NotificationCompat;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,9 +25,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void sendNotification(View view){
+        Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"));
+        PendingIntent pendingIntent= PendingIntent.getActivity(this, 0, intent, 0);
         // create notification in oncreate
         NotificationManager mNotificationManager= (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         NotificationCompat.Builder mBuilder= new NotificationCompat.Builder(this,CHANNEl_ID)
+                // akan melalukan intent ke action yang diberi yaitu http dicoding.com
+                .setContentIntent(pendingIntent)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 // muncul icon di sebelah kiri
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
@@ -47,5 +59,7 @@ public class MainActivity extends AppCompatActivity {
             mNotificationManager.notify(NOTIFICATION_ID, notification);
         }
     }
+
+
     // content intent digunakan ketika action notification di tekan
 }
